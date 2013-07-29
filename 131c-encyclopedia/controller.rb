5a49ff9ed0f91aeb676786a238b230d3a38ce4c5
Kyle Reierson <klreierson@yahoo.com>
2013-07-29 11:50:11 -0600

    committing class work

--- 131c-encyclopedia/controller.rb
+++ 131c-encyclopedia/controller.rb
@@ -1,5 +1,12 @@
 require '../dvc-sinatra.rb'
 
 get "/" do
+	@entries = EncyclopediaEntry.all
 	halt erb (:index)
 end
+
+get "/details/:id" do
+	@entry = EncyclopediaEntry.find(params:(:id))
+	halt erb (:details)
+end
+
